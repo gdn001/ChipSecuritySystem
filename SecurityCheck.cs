f890538452eb393aa87122b3979a4e0fbef56efa
gdn001 <gdn001@yahoo.com>
2026-05-11 09:43:35 -0400

    Update SecurityCheck.cs

--- SecurityCheck.cs
+++ SecurityCheck.cs
@@ -3,6 +3,16 @@ using System.Collections;
 
 namespace ChipSecuritySystem
 {
+	
+	/// <summary>
+    /// Take arraylist of chips called chipBag, the initial loop checks for starter chip based on the start color: blue.
+    /// It also checks for a one chip solution of blue:green
+    /// If blue chip found, set aside starter chip in solution list and send remaining chips to recursive function.
+    /// Recursive function will loop through the remain chips, find a connector chip, check if it is an end chip and correct solution.
+    /// Will continue to recurse through all of the chips until the best solution is found with the most chips.
+    /// </summary>
+    /// <param name="chipBag"></param>
+    /// <returns></returns>
     public class SecurityCheck
     {
         ArrayList bestSolution = new ArrayList();
@@ -38,6 +48,12 @@ namespace ChipSecuritySystem
             return bestSolution;
         }
 		
+		/// <summary>
+        /// Recursive function to check the remaining chips.
+        /// </summary>
+        /// <param name="chipBag"></param>
+        /// <param name="endColor"></param>
+        /// <param name="solutionChips"></param>		
 		private void checkRemainingBag(ArrayList chipBag, Color endColor, ArrayList solutionChips)
         {
             foreach (ColorChip chip in chipBag)
